2240859361306c7ddd6c2d2c4e82709ead4652ad
boo <dicksonagbesi@gmail.com>
2025-07-23 22:33:51 +0000

    main

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws"{
       region = "us-east-2"
 }
 
-resource "aws_instance" "guda"{
-   ami = "0d1b5a8c13042c939"
-   instance_type ="t2.micro"
+module "ec2_instance" {
+   ami_value = "0d1b5a8c13042c939"
+   instance_type_value ="t2.micro"
    tags ={
       Name = "my-evertf-module"
    }
