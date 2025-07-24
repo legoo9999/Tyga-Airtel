63bb1c17dbd23a288ace42fac176bd371566d589
Dickson K Agbesi <dicksonagbesi@gmail.com>
2025-07-24 00:52:01 +0000

    Update main.tf

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,7 +1,7 @@
 provider "aws"{
         region ="us-east-2"
 }
-resource "aws_instance" "guda"{
+resource "aws_instance" "proj"{
    ami ="var.ami_value"
    instance_type = "var.instance_type_value"
 }
