0830db1616c30f4fe8603a00a5d459de218c0420
boo <dicksonagbesi@gmail.com>
2025-07-29 01:44:07 +0000

    main

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ variable "aws_instance" {
 }
 
 module "ec2_instance" {
-   source = "modules/ec2_instance"
+   source = "./modules/ec2_instance"
    ami = var.ami
    instance_type = var.instance_type
 }
