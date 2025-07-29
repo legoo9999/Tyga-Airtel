ece0c93a055abeed32dbfbcab75d7650979fd7be
boo <dicksonagbesi@gmail.com>
2025-07-29 01:35:18 +0000

    main

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+variable "ami" {
+    description = "value"  
+}
+
+variable "aws_instance" {
+  description = "value"
+}
+
+module "ec2_instance" {
+   source = "modules/ec2_instance"
+   ami = var.ami
+   instance_type = var.instance_type
+}
---
+++ modules/ec2_instance/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+     region = "us-east-2"
+}
+
+variable "ami" {
+    description = "This is AMI for the insatnce"
+}
+
+variable "instance_type" {
+    description = "This shows the ins type example t2.micro" 
+}
+
+
+resource "aws_instance" "MyTworkspace"{
+    ami = "var.ami"
+    instance_type = "var.instance_type"
+}
